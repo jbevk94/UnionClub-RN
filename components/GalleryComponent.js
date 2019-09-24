12bac6eda29cc9cad571b80145cfad4e7f3661a3
Beverly James <jbevk94@gmail.com>
2019-09-23 18:54:38 -0700

    Updated and Added Gallery.js

--- components/GalleryComponent.js
+++ components/GalleryComponent.js
@@ -1,43 +1,39 @@
-import React, { Component } from 'react';
-import { Card } from 'react-native-elements';
-import { Text, ScrollView, View } from 'react-native';
+import React, { Component } from "react";
+import { Card } from "react-native-elements";
+import { Text, ScrollView, View } from "react-native";
 
 class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-        constructor(props) {
-            super(props);
-            this.state = {
-              
-            };   
-            
-        }
-    
-        static navigationOptions = {
-            title: 'Gallery'
-        };
+  static navigationOptions = {
+    title: "Gallery"
+  };
 
-        render() {
-            return(
-                <ScrollView>
-                    <Card title= "Contact Information">{
-                    <Text
-                        style={{margin: 10}}>
-                        121, Clear Water Bay Road{"\n"}
-
-Clear Water Bay, Kowloon{"\n"}
-
-HONG KONG {"\n"}
-
-Tel: +852 1234 5678{"\n"}
-
-Fax: +852 8765 4321{"\n"}
-
-Email:confusion@food.net</Text>
-                }
-                </Card>        
-                </ScrollView>
-            );
-        }
-    }
-
+  render() {
+    const renderGallery = ({ item, index }) => {
+      return (
+        <ListItem
+          key={index}
+          title={item.name}
+          subtitle={item.description}
+          leftAvatar={{ source: require("./images/alberto.png") }}
+        />
+      );
+    };
+    return (
+      <ScrollView>
+        <Card title="Gallery">
+          <FlatList
+            data={this.state.gallery}
+            renderItem={renderGallery}
+            keyExtractor={item => item.id.toString()}
+          />
+        </Card>
+      </ScrollView>
+    );
+  }
+}
 export default Gallery;
