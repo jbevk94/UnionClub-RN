58e25781a6754f89ba02a940c6c084919ca98e24
Beverly James <jbevk94@gmail.com>
2019-09-25 12:59:23 -0700

    Added Blog

---
+++ components/BlogComponent.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { Text, ScrollView, FlatList } from "react-native";
+import { Card, ListItem } from "react-native-elements";
+import { BLOGS } from "../shared/blogs";
+
+class Blog extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        blogs: BLOGS
+      };
+    }
+    static navigationOptions = {
+      title: "Blog"
+    };
+    render() {
+        const renderBlogger = ({item,index}) => {
+       return (
+           <ListItem
+          key={index}
+          title={item.title}
+          leftAvatar={{ source: { uri: item.avatar_url } }}
+          subtitle={item.description}
+          
+        />
+           );
+     };
+     return (
+       <ScrollView>
+           
+     <Card title="Coworking + Arts + Events">
+     <FlatList
+       data={this.state.blogs}
+       renderItem={renderBlogger}
+       keyExtractor={item => item.id.toString()}
+     />
+     </Card>
+           </ScrollView>
+     );}   
+         
+   }
+
+    export default Blog;
